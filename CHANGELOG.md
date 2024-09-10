cfa158f770e21715bdfb0703b7f099396d49e84a
Eric Saxby <sax@livinginthepast.org>
2024-09-10 10:30:35 -0700

    Release v1.3.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## Unreleased changes
 
+## 1.3.1
+
 - Correctly follow `navigate` attribute on a component when navigating from live to dead view.
 
 ## 1.3.0
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Pages.MixProject do
   use Mix.Project
 
   @scm_url "https://github.com/synchronal/pages"
-  @version "1.3.0"
+  @version "1.3.1"
 
   def application do
     [
